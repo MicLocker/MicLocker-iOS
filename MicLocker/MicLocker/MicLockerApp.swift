bed30f10151534af15867ea1b283a6f44936cffe
James McDougall <james.mcdougall@milwaukeetool.com>
2022-06-29 14:35:11 -0500

    Initial project commit, added app icon

---
+++ MicLocker/MicLocker/MicLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicLockerApp.swift
+//  MicLocker
+//
+//  Created by McDougall, James on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MicLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OpeningView()
+        }
+    }
+}
---
+++ MicLocker/MicLocker/OpeningView.swift
@@ -0,0 +1,20 @@
+//
+//  OpeningView.swift
+//  MicLocker
+//
+//  Created by McDougall, James on 6/29/22.
+//
+
+import SwiftUI
+
+struct OpeningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OpeningView_Previews: PreviewProvider {
+    static var previews: some View {
+        OpeningView()
+    }
+}
